57a696cc324772f77033b7e3be72f8e584cb32b8
RakeshRegala <150713451+RakeshRegala@users.noreply.github.com>
2025-04-16 07:44:03 +0530

    Update Single Source Shortest Path_AM.java

--- Single Source Shortest Path_AM.java
+++ Single Source Shortest Path_AM.java
@@ -1,9 +1,10 @@
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
 class ShortestPath {
-    static final int V = 9;
+    static final int V = 5;
     int minDistance(int dist[], Boolean sptSet[])
     {
         int min = Integer.MAX_VALUE, min_index = -1;
@@ -61,16 +62,13 @@ class ShortestPath {
     {
         /* Let us create the example graph discussed above
          */
-        int graph[][]
-            = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-                            { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-                            { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-                            { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-                            { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-                            { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-                            { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-                            { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-                            { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+       int graph[][] = new int[][] {
+            { 0, 10, 0, 5, 0 },
+            { 10, 0, 1, 2, 0 },
+            { 0, 1, 0, 0, 4 },
+            { 5, 2, 0, 0, 2 },
+            { 0, 0, 4, 2, 0 }
+        };
         ShortestPath t = new ShortestPath();
 
         // Function call
